35e538622515ed35679cd810f3859719de8f63a4
Markus Lehtonen <markus.lehtonen@intel.com>
2026-04-17 16:47:15 +0300

    cmd/sst: add tf (SST-TF) subcommand
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>

--- cmd/sst/main.go
+++ cmd/sst/main.go
@@ -46,6 +46,7 @@ type subCmd struct {
 var subCmds = map[string]subCmd{
 	"info": {description: "Print hierarchical SST information", f: subCmdInfo},
 	"bf":   {description: "Configure SST-BF feature", f: subCmdBF},
+	"tf":   {description: "Configure SST-TF feature", f: subCmdTF},
 	"cp":   {description: "Configure SST-CP feature", f: subCmdCP},
 }
 
@@ -322,6 +323,45 @@ func subCmdBF(args []string) error {
 		}
 	default:
 		return fmt.Errorf("unknown bf sub-command %q (valid: enable, disable)", sub)
+	}
+	return nil
+}
+
+func subCmdTF(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("usage: tf <enable|disable> [options]")
+	}
+	sub := args[0]
+	flags := flag.NewFlagSet("tf "+sub, flag.ExitOnError)
+	addGlobalFlags(flags)
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+	h, err := initHandle()
+	if err != nil {
+		return err
+	}
+	pkgs, err := getPackageHandles(h)
+	if err != nil {
+		return err
+	}
+	switch sub {
+	case "enable":
+		for _, pkg := range pkgs {
+			fmt.Printf("Enabling TF for package %d\n", pkg.ID())
+			if err := pkg.TFEnable(); err != nil {
+				return fmt.Errorf("failed to enable TF for package %d: %w", pkg.ID(), err)
+			}
+		}
+	case "disable":
+		for _, pkg := range pkgs {
+			fmt.Printf("Disabling TF for package %d\n", pkg.ID())
+			if err := pkg.TFDisable(); err != nil {
+				return fmt.Errorf("failed to disable TF for package %d: %w", pkg.ID(), err)
+			}
+		}
+	default:
+		return fmt.Errorf("unknown tf sub-command %q (valid: enable, disable)", sub)
 	}
 	return nil
 }
